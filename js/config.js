6b0135a210627f844089481ec0e3177d2aa23440
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-02-26 14:45:41 -0500

    V1.7 - XL Icons added, one other note.
    Music potentially being removed soon. See comments on line 18.

--- js/config.js
+++ js/config.js
@@ -3,18 +3,19 @@ var api_key = `e1f10a1e78da46f5b10a1e78da96f525`;
 // Input your mapbox key.
  var map_key = `pk.eyJ1IjoiaGFzdHl0dWJlIiwiYSI6ImNsa2hkZTh6bzAwazQzZHFyNmF5aTRsZGwifQ.5QJvYIHo0odZ5jCFApV7yw`
 
- var lat = "27.8006196"
- var lon = "-97.3966183"
- var icons = "2007"; // Types are 2007, 2010 (XL Coming Soon.)
+ var lat = "31.4618"
+ var lon = "-100.4426"
+ var icons = "XL"; // Types are 2007, 2010, and XL
 // Very Important:
 // If your icons is 2007, change to gif.
 // If your icons is 2010, change to png.
 // No period before the file type. Otherwise it gives it out as iconno..png/gif
-var filet = `gif` // gif or png. No period included
-var locationn =  "Corpus Christi" // The Name of your location.
-var units = "e" // The Units displayed. e is imperial, m is metric. Default is e.
+var filet = `png` // gif or png. No period included
+var locationn =  "San Angelo" // The Name of your location. Capital Letters are not required. 
+var units = "e" // The Units displayed. e is imperial, m is metric, h is hybrid. Default is e.
 // This is the message that appears on the marquee.
-var marquee_message = `Some random scroll bar on the simulator :)`
+var marquee_message = "The Weather Channel is the #1 Source for important weather information. Check out our website at weather.com and get weather information for your area! Also, check out our mobile app avaliable on iOS and Android! "
+// This Script may be removed soon due to the audio not always playing and the confusing configuration. You can play your own music via a file, or on the web!
 // Music
  var music = `Once and Again - Snuffy` // Your music. You need the exact name of the music file though. Example: High Tides of Maui - Ficara.
  var audiotype = `.wav` // Your audio format. Examples: .wav, .mp3, .mpeg, etc.
