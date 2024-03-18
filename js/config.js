448381de59360fe4a3b58aa23eafa25effaf35fc
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-03-18 12:07:33 -0400

    V1.9.1 Build 2

--- js/config.js
+++ js/config.js
@@ -5,22 +5,22 @@ var api_key = `e1f10a1e78da46f5b10a1e78da96f525`;
 // Input your mapbox key.
  var map_key = `pk.eyJ1IjoiaGFzdHl0dWJlIiwiYSI6ImNsa2hkZTh6bzAwazQzZHFyNmF5aTRsZGwifQ.5QJvYIHo0odZ5jCFApV7yw`
 
- var lat = "39.045096"
- var lon = "125.5973578"
+ var lat = "24.555358"
+ var lon = "-81.7799769"
  var icons = "2010"; // Types are 2007, 2010, and XL
- var bg = "bg1" // Insert your custom background. bg1 is the default. Put it in images/bg folder
+ var bg = "beach" // Insert your custom background. bg1 is the default. Put it in images/bg folder
  var bgformat = "png" // If your image is other than png, set the image file type. Do not include a dot before the file type.
 // Very Important:
 // If your icons is 2007, change to gif.
 // If your icons is 2010, change to png.
 // No period before the file type. Otherwise it gives it out as 26..png/gif
 var filet = `png` // gif or png. No period included
-var locationn =  "평양" // Your Location.
-var forecastlocation = `평양지역` // The Location that appears on the forecast. You can do ${locationn} for the city name above
-var almanacstationname = "평양국제공항"
+var locationn =  "Key West" // Your Location.
+var forecastlocation = `Florida Keys` // The Location that appears on the forecast. You can do ${locationn} for the city name above
+var almanacstationname = "Key West"
 var units = "e" // The Units displayed. e is imperial, m is metric, h is hybrid, s is (the) kelvin. Default is e.
 // This is the message that appears on the marquee.
-var marquee_message = "Meh, some scroll."
+var marquee_message = "Don't know what to put here.."
 // The Music Configuration Removed was on March 4th 2024.
 
  // Alert Configuration
