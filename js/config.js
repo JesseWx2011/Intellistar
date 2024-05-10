7ada53b6ac0b6f7a81ad562d5ddd398c4d3d6d76
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-05-10 10:30:03 -0400

    Fixes

--- js/config.js
+++ js/config.js
@@ -5,8 +5,9 @@ var api_key = `e1f10a1e78da46f5b10a1e78da96f525`;
 // Input your mapbox key.
  var map_key = `pk.eyJ1IjoiaGFzdHl0dWJlIiwiYSI6ImNsa2hkZTh6bzAwazQzZHFyNmF5aTRsZGwifQ.5QJvYIHo0odZ5jCFApV7yw`
 
- var lat = "38.889654"
- var lon = "-77.036458"
+
+ var lat = "38.44"
+ var lon = "-75.43"
  var icons = "2010"; // Types are 2007, 2010, and XL
  var bg = "bg1" // Insert your custom background. bg1 is the default. Put it in images/bg folder
  var bgformat = "png" // If your image is other than png, set the image file type. Do not include a dot before the file type.
@@ -15,11 +16,12 @@ var api_key = `e1f10a1e78da46f5b10a1e78da96f525`;
 // If your icons is 2010, change to png.
 // No period before the file type. Otherwise it gives it out as 26..png/gif
 var filet = `png` // gif or png. No period included
-var locationn =  "Washington DC" // Your Location.
-var forecastlocation = `District of Columbia` // The Location that appears on the forecast. You can do ${locationn} for the city name above
-var almanacstationname = "Regan Nat'l Airport"
-var units = "e" // The Units displayed. e is imperial, m is metric, h is hybrid, s is (the) kelvin. Default is e.
-// This is the message that appears on the LDL Bar.
+var locationn =  "" // Your Location. Leave Blank for autolocation.
+var forecastlocation = `Salisbury` // The Location that appears on the forecast. You can do ${locationn} for the city name above
+var almanacstationname = "" /* The Name that appears on the Almanac Slide. Leave blank if 
+you want to automatically set the name of the station */
+var units = "e" // The displayed. e is imperial, m is metric, h is hybrid, s is (the) kelvin. Default is e.
+// This is the message that aUnits ppears on the LDL Bar.
 var marquee_message = "It is going to be hot on Sunday and Monday! It summer already! And a chance for severe thunderstorms.."
 
 // The Music Configuration Removed was on March 4th 2024.
