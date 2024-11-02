3c5e63ab0379dd509caff40e31c8f62d80dbba43
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-11-02 10:20:28 -0500

    Fufill Request for Halloween20/Github

--- js/config.js
+++ js/config.js
@@ -7,15 +7,15 @@ var api_key = `e1f10a1e78da46f5b10a1e78da96f525`;
  var map_key = `pk.eyJ1IjoiaGFzdHl0dWJlIiwiYSI6ImNsa2hkZTh6bzAwazQzZHFyNmF5aTRsZGwifQ.5QJvYIHo0odZ5jCFApV7yw`
 
  // This is to automatically transition the slides. Viewer Requested. 
- automatictransition = "true"
+automatictransition = "true"
 
-var timezone = "America/New_York"
+var timezone = "America/Chicago"
 // Location Configuration
- var lat = "28.4831649" // Latitude. You can head over to www.google.com/maps/ and right click on a spot on the map for your cordinates.
- var lon = "-82.53698" // Longitude.
+ var lat = "30.4213199" // Latitude. You can head over to www.google.com/maps/ and right click on a spot on the map for your cordinates.
+ var lon = "-87.217067" // Longitude.
  // Icon Configuration
- var icons = "XL"; // Types are 2007, 2010, and XL
- var filet = `png` // gif or png. No period included
+ var icons = "2007"; // Types are 2007, 2010, and XL
+ var filet = `gif` // gif or png. No period included
 
  // Backgrounds
  var bg = "bg1" // Insert your custom background. bg1 is the default. Put it in images/bg folder
@@ -24,13 +24,13 @@ var timezone = "America/New_York"
 // If your icons is 2007, change to gif.
 // If your icons is 2010, change to png.
 // No period before the file type. Otherwise it gives it out as 26..png/gif
-var locationn =  "Spring Hill" // Your Location. Leave Blank for autolocation.
-var forecastlocation = `Hernando County` // The Location that appears on the forecast. You can do ${locationn} for the city name above
-var almanacstationname = "" /* The Name that appears on the Almanac Slide. Leave blank if 
+var locationn =  "Pensacola" // Your Location. Leave Blank for autolocation.
+var forecastlocation = `Pensacola Metro` // The Location that appears on the forecast. You can do ${locationn} for the city name above
+var almanacstationname = "Pensacola International" /* The Name that appears on the Almanac Slide. Leave blank if 
 you want to automatically set the name of the station */
 var units = "e" // The displayed. e is imperial, m is metric, h is hybrid, s is (the) kelvin. Default is e.
 // This is the message that aUnits ppears on the LDL Bar.
-var marquee_message = "Tropical Storm Debby is approaching as of August 3rd, 2024. I wonder if I will even update this. Stay safe Y'all!"
+var marquee_message = "As Requested by Halloween20 on GitHub, the location has been set to Pensacola, Florida! "
 
 // The Music Configuration was Removed on March 4th 2024.
 
