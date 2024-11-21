4706f2a5217b50f4fb72b82fb40c363cdb5d11f9
Jesse Hasty <134172009+JesseWx2011@users.noreply.github.com>
2024-11-21 13:03:34 -0600

    v1.9.8

--- js/alerts.js
+++ js/alerts.js
@@ -15,6 +15,8 @@ FetchAlerts();
   var alert = features[0].properties.event
   console.log(alert)
   // Script Implemented on March 6th 2024.
+
+
   if (features[0].properties.severity = "Extreme") {
     document.getElementById("nmarquee").innerHTML = `<div class="alertmarquee" id="nwsalert"  style="color: white;">${features[0].properties.event}</div>
     <marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="6" scrolldelay="100" float="left">${features[0].properties.description}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/warning.png">`
@@ -26,7 +28,6 @@ FetchAlerts();
 
   } if (features[0].properties.severity = "Moderate") {
     document.getElementById("alerttext").innerHTML = `${features[0].properties.event} in effect from ${starttime} until ${endtime}`
-
     document.getElementById("nmarquee").innerHTML = `
     <div class="alertmarquee" id="nwsalert"  style="color: black;">${features[0].properties.event}</div>
     <marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="7">${features[0].properties.parameters.NWSheadline[0]}. ${features[0].properties.description}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/watch-advisory.png">`
@@ -38,12 +39,39 @@ FetchAlerts();
     <marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="7">${features[0].properties.parameters.NWSheadline[0]}. ${features[0].properties.description}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/watch-advisory.png">`
   document.getElementById("time").style.color = "white"
   document.getElementById("alerttext").innerHTML = `${features[0].properties.event} in effect from ${starttime} until ${endtime}`
-  } else {
+  } 
+  if (ffeatures[1].properties.severity.length >= 1) {
+    if (features[1].properties.severity = "Extreme") {
+      document.getElementById("nmarquee").innerHTML = `<div class="alertmarquee" id="nwsalert"  style="color: white;">${features[1].properties.event}</div>
+      <marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="6" scrolldelay="100" float="left">${features[1].properties.description}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/warning.png">`
+      document.getElementById("time").style.color = "white";
+      document.getElementById("alerttext").innerHTML = `${features[1].properties.event} in effect from ${starttime} until ${endtime}`
+     } if (features[1].properties.severity = "Severe") {
+      document.getElementById("nmarquee").innerHTML = `<div class="alertmarquee" id="nwsalert" style="color: white;">${features[1].properties.event}</div><marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="6" scrolldelay="100" float="left">${features[1].properties.parameters.NWSheadline[1]}. ${features[1].properties.description}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/warning.png">`
+      document.getElementById("time").style.color = "white";
+  
+    } if (features[1].properties.severity = "Moderate") {
+      document.getElementById("alerttext").innerHTML = `${features[1].properties.event} in effect from ${starttime} until ${endtime}`
+      document.getElementById("nmarquee").innerHTML = `
+      <div class="alertmarquee" id="nwsalert"  style="color: black;">${features[1].properties.event}</div>
+      <marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="7">${features[1].properties.parameters.NWSheadline[1]}. ${features[1].properties.description}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/watch-advisory.png">`
+       document.getElementById("time").style.color = "white"
+    } if (features[1].properties.severity = "Minor") {
+  
+      document.getElementById("nmarquee").innerHTML = `
+      <div class="alertmarquee" id="nwsalert"  style="color: black;">${features[1].properties.event}</div>
+      <marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="7">${features[1].properties.parameters.NWSheadline[1]}. ${features[1].properties.description}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/watch-advisory.png">`
+    document.getElementById("time").style.color = "white"
+    document.getElementById("alerttext").innerHTML = `${features[1].properties.event} in effect from ${starttime} until ${endtime}`
+    } 
+  }
+   else {
     document.getElementById("nmarquee").innerHTML = `<marquee class="marquee" id="scrollingmarquee" style="text-transform: uppercase;font-size: 29px;text-shadow: 2px 1px black;" scrollamount="7">${marquee_message}</marquee><img id="marqueebar" class="scroll" src="./images/ldl/LDLBar.png">`
     document.getElementById("time").style.color = "black"
   }
 
 
+  
    }
 
   FetchAlerts();
--- js/radar.js
+++ js/radar.js
@@ -1,21 +1,21 @@
 // Fetch Radar title imagery
-  map_img = `https://api.mapbox.com/styles/v1/hastytube/clli6kx8u004m01p28nb47zz2/static/${lon},${lat},8/1280x483?access_token=${map_key}`
-  radar_url = `https://maps.aerisapi.com/wgE96YE3scTQLKjnqiMsv_sFA4Gfz8s8brxUUHoZTQ3iZIRy6YSwLhf1wJQzDi/radar-est:75,admin/1280x483/${lat},${lon},8/latest.jpg`
-    document.getElementById("mapboxmap").innerHTML = `<img src="${map_img}"></img>`
- function animate() {
-  frame1 = `https://maps.aerisapi.com/wgE96YE3scTQLKjnqiMsv_sFA4Gfz8s8brxUUHoZTQ3iZIRy6YSwLhf1wJQzDi/radar-est:75,admin/1280x483/${lat},${lon},8/-25min.jpg`
-  frame2 = `https://maps.aerisapi.com/wgE96YE3scTQLKjnqiMsv_sFA4Gfz8s8brxUUHoZTQ3iZIRy6YSwLhf1wJQzDi/radar-est:75,admin/1280x483/${lat},${lon},8/-20min.jpg`
-  frame3 = `https://maps.aerisapi.com/wgE96YE3scTQLKjnqiMsv_sFA4Gfz8s8brxUUHoZTQ3iZIRy6YSwLhf1wJQzDi/radar-est:75,admin/1280x483/${lat},${lon},8/-15min.jpg`
-  frame4 = `https://maps.aerisapi.com/wgE96YE3scTQLKjnqiMsv_sFA4Gfz8s8brxUUHoZTQ3iZIRy6YSwLhf1wJQzDi/radar-est:75,admin/1280x483/${lat},${lon},8/-10min.jpg`
-  frame5 = `https://maps.aerisapi.com/wgE96YE3scTQLKjnqiMsv_sFA4Gfz8s8brxUUHoZTQ3iZIRy6YSwLhf1wJQzDi/radar-est:75,admin/1280x483/${lat},${lon},8/-5min.jpg`
-  frame6 = `https://maps.aerisapi.com/wgE96YE3scTQLKjnqiMsv_sFA4Gfz8s8brxUUHoZTQ3iZIRy6YSwLhf1wJQzDi/radar-est:75,admin/1280x483/${lat},${lon},8/latest.jpg`
+map_img = `https://api.mapbox.com/styles/v1/hastytube/clli6kx8u004m01p28nb47zz2/static/${lon},${lat},8/1280x483?access_token=${map_key}`
+radar_url = `https://maps.aerisapi.com/DZLMGEFxCvfbQRG7aSN3c_N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt/radar-est:75,admin/1280x483/${lat},${lon},8/latest.jpg`
+  document.getElementById("mapboxmap").innerHTML = `<img src="${map_img}"></img>`
+function animate() {
+frame1 = `https://maps.aerisapi.com/DZLMGEFxCvfbQRG7aSN3c_N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt/radar-est:75,admin/1280x483/${lat},${lon},8/-25min.jpg`
+frame2 = `https://maps.aerisapi.com/DZLMGEFxCvfbQRG7aSN3c_N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt/radar-est:75,admin/1280x483/${lat},${lon},8/-20min.jpg`
+frame3 = `https://maps.aerisapi.com/DZLMGEFxCvfbQRG7aSN3c_N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt/radar-est:75,admin/1280x483/${lat},${lon},8/-15min.jpg`
+frame4 = `https://maps.aerisapi.com/DZLMGEFxCvfbQRG7aSN3c_N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt/radar-est:75,admin/1280x483/${lat},${lon},8/-10min.jpg`
+frame5 = `https://maps.aerisapi.com/DZLMGEFxCvfbQRG7aSN3c_N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt/radar-est:75,admin/1280x483/${lat},${lon},8/-5min.jpg`
+frame6 = `https://maps.aerisapi.com/DZLMGEFxCvfbQRG7aSN3c_N63dulcmKzQTrWjIrTe2aGKmOw5AhERWWUmjHQKt/radar-est:75,admin/1280x483/${lat},${lon},8/latest.jpg`
 
-  setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame1}"></img>`, 500)
-  setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame2}"></img>`, 1000)
-  setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame3}"></img>`, 1500)
-  setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame4}"></img>`, 2000)
-  setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame5}"></img>`, 2500)
-  setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame6}"></img>`, 3000)
+setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame1}"></img>`, 500)
+setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame2}"></img>`, 1000)
+setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame3}"></img>`, 1500)
+setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame4}"></img>`, 2000)
+setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame5}"></img>`, 2500)
+setTimeout(document.getElementById("mapboxmap").innerHTML = `<img src="${frame6}"></img>`, 3000)
 
- }
- animate()
+}
+animate()
